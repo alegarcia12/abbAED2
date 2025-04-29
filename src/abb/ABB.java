8301e18adcfcf301cecd7848a054072c7066b927
Alejandro Garcia <65517825+alegarcia12@users.noreply.github.com>
2025-04-29 19:49:18 -0300

    Ejercicio 6 parte E terminada

--- src/abb/ABB.java
+++ src/abb/ABB.java
@@ -1,5 +1,8 @@
 package abb;
 
+import lista.ILista;
+import lista.Lista;
+
 public class ABB<T extends Comparable<T>> {
 
     private NodoABB<T> raiz;
@@ -49,23 +52,141 @@ public class ABB<T extends Comparable<T>> {
 
     //EJERCICIO 6 PARTE A
 
-    public int contarElemMayoresAK(T k){
+    public int contarElemMayoresAK(T k) {
         return contarElemMayoresAK(raiz, k);
     }
 
-    private int contarElemMayoresAK(NodoABB<T> nodo, T k){
-        if (nodo != null){
-            if (k.compareTo(nodo.dato) >= 0){
-                return contarElemMayoresAK(nodo.der,k);
-            }else{
-                return contarElemMayoresAK(nodo.izq,k) + 1 + contarElemMayoresAK(nodo.der,k);
+    private int contarElemMayoresAK(NodoABB<T> nodo, T k) {
+        if (nodo != null) {
+            if (k.compareTo(nodo.dato) >= 0) {
+                return contarElemMayoresAK(nodo.der, k);
+            } else {
+                return contarElemMayoresAK(nodo.izq, k) + 1 + contarElemMayoresAK(nodo.der, k);
             }
-        }else{
+        } else {
             return 0;
         }
     }
 
+    //EJERCICIO 6 PARTE A CON String
+    public String listarElemMayoresAK(T k) {
+        return listarElemMayoresAK(raiz, k);
+    }
 
+    private String listarElemMayoresAK(NodoABB<T> nodo, T k) {
+        if (nodo != null) {
+            if (k.compareTo(nodo.dato) >= 0) {
+                return listarElemMayoresAK(nodo.der, k);
+            } else {
+                return listarElemMayoresAK(nodo.izq, k) + nodo.dato + " " + listarElemMayoresAK(nodo.der, k);
+            }
+        } else {
+            return "";
+        }
+    }
+
+    //EJERCICIO 6 PARTE B
+
+    public ILista<T> listarElementosAsc() {
+        ILista<T> elementosOrdenados = new Lista<>();
+        listarAscendente(raiz, elementosOrdenados);
+        return elementosOrdenados;
+    }
+
+    private void listarAscendente(NodoABB<T> nodo, ILista<T> elems) {
+        if (nodo != null) {
+            listarAscendente(nodo.der, elems);
+            elems.insertar(nodo.dato);
+            listarAscendente(nodo.izq, elems);
+        }
+    }
+
+    //EJERCICIO 6 PARTE C
+    /*
+    Desarrolle un algoritmo que, recibiendo un valor entero k, retorne una
+    lista con los elementos que son mayores a k.
+    */
+    public ILista<T> obtenerListaMayoresAK(T k) {
+        ILista<T> elementosMayoresAK = new Lista<>();
+        listarMayoresaKA(raiz, elementosMayoresAK, k);
+        return elementosMayoresAK;
+    }
+
+    private void listarMayoresaKA(NodoABB<T> nodo, ILista<T> elementosMayoresAK, T k) {
+        if (nodo != null) {
+            if (k.compareTo(nodo.dato) >= 0) {
+                listarMayoresaKA(nodo.der, elementosMayoresAK, k);
+            } else {
+                elementosMayoresAK.insertar(nodo.dato);
+                listarMayoresaKA(nodo.izq, elementosMayoresAK, k);
+                listarMayoresaKA(nodo.der, elementosMayoresAK, k);
+            }
+        }
+    }
+
+    //EJERCICIO 6 PARTE D
+    /*
+     * Desarrolle un algoritmo que, recibiendo un valor entero, retorne el
+     * nivel en que se encuentra dicho valor o -1 si no se encuentra.
+     * */
+
+    public int obtenerNivelDe(T k) {
+        return obtenerNivelDe(raiz, 0, k);
+    }
+
+    private int obtenerNivelDe(NodoABB<T> nodo, int nivel, T k) {
+        if (nodo != null) {
+            if (k.equals(nodo.dato)) {
+                return nivel;
+            } else if (k.compareTo(nodo.dato) < 0) {
+                return obtenerNivelDe(nodo.izq, nivel + 1, k);
+            } else {
+                return obtenerNivelDe(nodo.der, nivel + 1, k);
+            }
+        } else {
+            return -1;
+        }
+    }
+
+    //EJERCICIO 6 PARTE E
+    /*
+    Desarrolle un algoritmo que, recibiendo un valor
+    entero k, imprima los elementos del nivel k.
+     * */
+
+    public void ej6parteE(T k) {
+        ej6parteE(raiz, k, 0);
+    }
+
+    public void ej6parteE(NodoABB<T> nodo, T nivel, int contador) {
+        if (nodo != null) {
+            if (contador == (Integer) nivel) {
+                System.out.print(nodo.dato + " ");
+            } else {
+                ej6parteE(nodo.der, nivel, contador + 1);
+                ej6parteE(nodo.izq, nivel, contador + 1);
+            }
+        }
+    }
+
+    //EJERCICIO 6 PARTE F
+    /*
+    Desarrolle un algoritmo que imprima
+    el ABB en orden por niveles.
+    */
+
+    public void ej6parteF(){
+        ej6parteF(raiz);
+    }
+
+    private void ej6parteF(NodoABB<T> nodo) {
+        if (nodo != null){
+            System.out.println(nodo.dato);
+        }
+    }
+
+
+    //CLASE DE NODO
     private class NodoABB<Q> {
         private Q dato;
         private NodoABB izq;
@@ -74,7 +195,6 @@ public class ABB<T extends Comparable<T>> {
         public NodoABB(Q dato) {
             this.dato = dato;
         }
-
 
         @Override
         public String toString() {
---
+++ src/mainEj6.java
@@ -0,0 +1,33 @@
+import abb.ABB;
+import lista.ILista;
+
+public class mainEj6 {
+    public static void main(String[] args) {
+
+        int[] enteros = {10, 6, 18, 4, 9, 15, 21, 16};
+
+        ABB<Integer> abbEnteros = new ABB<>();
+
+        for (int i : enteros) {
+            abbEnteros.insertar(i);
+        }
+
+        //System.out.println("Cantidad de elementos mayores a 10: " + abbEnteros.contarElemMayoresAK(10));
+        //System.out.println("Cantidad de elementos mayores a 9: " + abbEnteros.contarElemMayoresAK(9));
+        // System.out.println("Cantidad de elementos mayores a 8: " + abbEnteros.contarElemMayoresAK(8));
+        //System.out.println("Lista de elementos mayores a 8: " + abbEnteros.listarElemMayoresAK(8));
+
+//        ILista<Integer> elemsOrdenados = abbEnteros.listarElementosAsc();
+//        System.out.println("Elementos de la lista elemsOrdenados: ");
+//        elemsOrdenados.imprimirDatos();
+
+//        ILista<Integer> elemsOrdenadosMayoresAK = abbEnteros.obtenerListaMayoresAK(10);
+//        System.out.println("Elementos de la lista elemsOrdenados: ");
+//        elemsOrdenadosMayoresAK.imprimirDatos();
+
+        //System.out.println(abbEnteros.obtenerNivelDe(9));
+
+        //abbEnteros.ej6parteE(3);
+        abbEnteros.ej6parteF();
+    }
+}
